bac50704e3e290d64e0bf36e8204c1e489553731
mohammed kaif <mkaif555123@gmail.com>
2024-05-07 21:37:13 +0500

    100% WORK DONE

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// function bulboff() {
-//   document.getElementById("image").src =
-//     "https://media.istockphoto.com/id/502789173/photo/light-bulb-concept.jpg?s=612x612&w=0&k=20&c=vJfSDuFJf_4UsQcUsdMNUfDkkNU6Bf9cTHF-n4-Zwvs=";
-// }
-// function bulbon() {
-//   document.getElementById("image").src =
-//     "https://img.freepik.com/premium-vector/incandescent-light-bulb-black-background-glowing-lightbulb-realistic-vector-illustration_261737-778.jpg";
-// }
+function bulboff() {
+  document.getElementById("image").src =
+    "https://media.istockphoto.com/id/502789173/photo/light-bulb-concept.jpg?s=612x612&w=0&k=20&c=vJfSDuFJf_4UsQcUsdMNUfDkkNU6Bf9cTHF-n4-Zwvs=";
+}
+function bulbon() {
+  document.getElementById("image").src =
+    "https://img.freepik.com/premium-vector/incandescent-light-bulb-black-background-glowing-lightbulb-realistic-vector-illustration_261737-778.jpg";
+}
