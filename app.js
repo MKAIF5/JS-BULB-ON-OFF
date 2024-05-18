5fbf4785ad28eed004585ee551ef956ec5b6c17a
MOHAMMED KAIF <146006914+MKAIF5@users.noreply.github.com>
2024-05-18 21:53:28 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 function bulboff() {
-  document.getElementById("image").src =
-    "https://media.istockphoto.com/id/502789173/photo/light-bulb-concept.jpg?s=612x612&w=0&k=20&c=vJfSDuFJf_4UsQcUsdMNUfDkkNU6Bf9cTHF-n4-Zwvs=";
+  document.getElementById("image").src = "https://media.istockphoto.com/id/502789173/photo/light-bulb-concept.jpg?s=612x612&w=0&k=20&c=vJfSDuFJf_4UsQcUsdMNUfDkkNU6Bf9cTHF-n4-Zwvs=";
 }
 function bulbon() {
   document.getElementById("image").src =
